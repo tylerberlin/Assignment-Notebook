a8068167c789cf260cf4c16b0f78f5eca46b4777
tyler0611 <berlity0611@gmail.com>
2025-01-24 11:54:27 -0600

    Main Assignemt Notebook

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Tyler Berlin on 1/24/25.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAssignmentView()
+}
---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,8 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Tyler Berlin on 1/24/25.
+//
+
+import Foundation
